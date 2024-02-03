baae477b09d6cd4eaf1cd7b311c76fffb63d5ac3
P.K Selani Didualni <pkselani00@gmail.com>
2024-02-03 07:21:12 -0800

    initial

---
+++ Learn/First.java
@@ -0,0 +1,7 @@
+//Class name always should start with capital letter
+//case sensitive
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
